a3f5a0f37a6a4be293160a7dcc0fc290e67781d2
sebastian073 <116037303+sebastian073@users.noreply.github.com>
2025-04-10 01:33:06 +0000

    appi4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AppProvider } from './contexto/contexto';
+import './App.css'
 
-import Menu from './componentes/menu'
-import Aleatorios from './componentes/aleatorios';
+import menu from './componentes/menu'
+import aleatorios from './componentes/aleatorios';
 import lista from './componentes/lista';
-import Capturados from './componentes/capturados';
-import Favoritos from './componentes/favoritos';
-import Usuarios from './componentes/usuarios';
-import Detalle from './componentes/detalle';
+import favoritos from './componentes/favoritos';
+import usuarios from './componentes/usuarios';
+import detalle from './componentes/detalle';
+import original from './componentes/original';
+
 
 function App() {
 
   return (
-    <AppProvider>
     <Router>
       <Menu />
 
@@ -21,13 +21,12 @@ function App() {
         <Route path="/" element={<Lista />} />
         <Route path="/usuarios" element={<Usuarios />} />
         <Route path="/aleatorios" element={<Aleatorios />} />
-        <Route path="/capturados" element={<Capturados />} />
+        <Route path="/original" element={<Capturados />} />
         <Route path="/favoritos" element={<Favoritos />} />
         <Route path="/detalle/:name" element={<Detalle />} />
       </Routes>
 
     </Router>
-    </AppProvider>
   );
 }
 
